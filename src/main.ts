777fe504e62ca6933895ff87db14919f3954b91d
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2022-04-08 00:04:15 +0800

    chore: init commit

---
+++ src/main.ts
@@ -0,0 +1,89 @@
+import { createApp } from "vue";
+import {
+  ComponentClass,
+  createElement,
+  Fragment,
+  FunctionComponent,
+  useState,
+} from "react";
+import { createRoot } from "react-dom/client";
+import { defineComponent, h, ref, watch } from "vue";
+
+function react2vue(
+  reactComponent: FunctionComponent<any> | ComponentClass<any>,
+  props?: string[]
+) {
+  return defineComponent({
+    props: props ?? [],
+    setup(props) {
+      const el = ref<HTMLDivElement>();
+
+      watch([props, el], ([props, el], _, onCleanup) => {
+        const root = createRoot(el as HTMLDivElement);
+        const reactElement = createElement(reactComponent, props);
+        root.render(reactElement);
+        onCleanup(() => root.unmount());
+      });
+
+      return { el };
+    },
+    render() {
+      // @ts-ignore
+      return h("div", {
+        ref: (el: HTMLDivElement) => (this.el = el),
+      });
+    },
+  });
+}
+
+function Printer({ text }: { text: string }) {
+  return createElement("p", null, `React component: ${text}`);
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return createElement(Fragment, null, [
+    createElement(
+      "button",
+      {
+        onClick: () => setCount(count + 1),
+      },
+      "+"
+    ),
+    createElement("span", null, count),
+    createElement(
+      "button",
+      {
+        onClick: () => setCount(count - 1),
+      },
+      "-"
+    ),
+  ]);
+}
+
+const ReactPrinter = react2vue(Printer, ["text"]);
+const ReactCounter = react2vue(Counter);
+
+const App = defineComponent({
+  setup() {
+    const text = ref("114514");
+    return {
+      text,
+      onTextChange(e: any) {
+        text.value = e.target.value;
+      },
+    };
+  },
+  render() {
+    const { text, onTextChange } = this;
+    return [
+      h("input", { oninput: onTextChange, value: text }),
+      h("p", null, `Vue Component: ${text}`),
+      h(ReactPrinter, { text }),
+      h(ReactCounter),
+    ];
+  },
+});
+
+createApp(App).mount("#app");
